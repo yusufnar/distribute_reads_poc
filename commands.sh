d80784cda7b013c28bf9d51b232a66bea81443a8
Yusuf Nar <yusufnar@gmail.com>
2026-04-21 13:00:13 +0300

    doc: addd

--- commands.sh
+++
@@ -1,6 +0,0 @@
-curl http://localhost:3000/api/db_info
-
-docker compose exec rails-app getent hosts pg-replicas
-
-docker run --rm --network distribute_reads_poc_default alpine sh -c "apk add --no-cache bind-tools >/dev/null && dig pg-replicas +short"
-
--- queries_and_commands.sql
+++ queries_and_commands.sql
@@ -4,4 +4,13 @@ docker exec pg-replica1 psql -U postgres -d appdb -t -c "SELECT state, query, co
 
 docker exec pg-replica1 psql -U postgres -d appdb -t -c "SELECT calls, total_exec_time, mean_exec_time, rows, query FROM pg_stat_statements ORDER BY mean_exec_time DESC;"
 
- docker exec pg-primary psql -U postgres -d appdb -c "\dx" 
+docker exec pg-primary psql -U postgres -d appdb -c "\dx" 
+
+curl http://localhost:3000/api/db_info
+
+docker compose exec rails-app getent hosts pg-replicas
+
+docker run --rm --network distribute_reads_poc_default alpine sh -c "apk add --no-cache bind-tools >/dev/null && dig pg-replicas +short"
+
+docker compose exec rails-app bin/rails runner 'ActiveRecord::Base.clear_all_connections!; puts :cleared'
+
